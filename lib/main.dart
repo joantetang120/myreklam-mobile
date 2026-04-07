7def02d8f19b4d5856c198d5021953ac9cbf98b7
joantetang120 <joantetang120@gmail.com>
2026-04-07 15:22:04 +0100

    fixed changes before merge

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,7 @@ class MyApp extends StatelessWidget {
         routes: {
           '/login': (context) => const LoginScreen(),
         },
-        home: const LoginScreen(),
+        home: const SplashScreen(),
       ),
     );
   }
